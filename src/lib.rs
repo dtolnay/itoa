7e81b0290a92d5bea58bd5912237ccc376704186
David Tolnay <dtolnay@gmail.com>
2019-05-01 18:23:44 -0700

    Remove unnecessary license header

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,3 @@
-// Copyright 2016 Itoa Developers
-//
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
-
 #![doc(html_root_url = "https://docs.rs/itoa/0.4.3")]
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(
