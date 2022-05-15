f3485c0ef72a859754246edde8379dd67a3547dd
David Tolnay <dtolnay@gmail.com>
2022-05-15 14:09:51 -0700

    Release 1.0.2

--- src/lib.rs
+++ src/lib.rs
@@ -30,7 +30,7 @@
 //!
 //! ![performance](https://raw.githubusercontent.com/dtolnay/itoa/master/performance.png)
 
-#![doc(html_root_url = "https://docs.rs/itoa/1.0.1")]
+#![doc(html_root_url = "https://docs.rs/itoa/1.0.2")]
 #![no_std]
 #![allow(
     clippy::cast_lossless,
