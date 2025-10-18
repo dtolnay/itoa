6e71a7c3a3d4ba59afadee5a098aba8ec1791147
David Tolnay <dtolnay@gmail.com>
2025-10-18 08:36:32 -0700

    Relocate clippy non_canonical_clone_impl suppression
    Since nightly-2025-10-18 the lint is posted on the impl block, not on
    the trait method.

--- src/lib.rs
+++ src/lib.rs
@@ -73,9 +73,9 @@ impl Default for Buffer {
 
 impl Copy for Buffer {}
 
+#[allow(clippy::non_canonical_clone_impl)] // false positive https://github.com/rust-lang/rust-clippy/issues/11072
 impl Clone for Buffer {
     #[inline]
-    #[allow(clippy::non_canonical_clone_impl)] // false positive https://github.com/rust-lang/rust-clippy/issues/11072
     fn clone(&self) -> Self {
         Buffer::new()
     }
