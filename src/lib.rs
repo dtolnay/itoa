e2059368d79bde28b593f6e5605494d6d7ded118
David Tolnay <dtolnay@gmail.com>
2025-12-13 20:05:33 -0800

    Resolve borrow_as_ptr pedantic clippy lints
        warning: borrow as raw pointer
          --> src/lib.rs:99:19
           |
        99 |             &mut *(&mut self.bytes as *mut [MaybeUninit<u8>; i128::MAX_STR_LEN])
           |                   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ help: try: `core::ptr::addr_of_mut!(self.bytes)`
           |
           = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#borrow_as_ptr
           = note: `-W clippy::borrow-as-ptr` implied by `-W clippy::pedantic`
           = help: to override `-W clippy::pedantic` add `#[allow(clippy::borrow_as_ptr)]`

--- src/lib.rs
+++ src/lib.rs
@@ -96,8 +96,7 @@ impl Buffer {
     #[cfg_attr(feature = "no-panic", no_panic)]
     pub fn format<I: Integer>(&mut self, i: I) -> &str {
         let string = i.write(unsafe {
-            &mut *(&mut self.bytes as *mut [MaybeUninit<u8>; i128::MAX_STR_LEN])
-                .cast::<<I as private::Sealed>::Buffer>()
+            &mut *ptr::addr_of_mut!(self.bytes).cast::<<I as private::Sealed>::Buffer>()
         });
         if string.len() > I::MAX_STR_LEN {
             unsafe { hint::unreachable_unchecked() };
