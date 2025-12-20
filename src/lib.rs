7d6c0819891118b3ea9631fc7fde264f76142658
David Tolnay <dtolnay@gmail.com>
2025-12-20 14:12:40 -0800

    Ignore identity_op clippy lint
        warning: this operation has no effect
           --> src/lib.rs:382:17
            |
        382 |             buf[offset + 0].write(DECIMAL_PAIRS[pair1 * 2 + 0]);
            |                 ^^^^^^^^^^ help: consider reducing it to: `offset`
            |
            = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#identity_op
            = note: `-W clippy::identity-op` implied by `-W clippy::all`
            = help: to override `-W clippy::all` add `#[allow(clippy::identity_op)]`
    
        warning: this operation has no effect
           --> src/lib.rs:382:49
            |
        382 |             buf[offset + 0].write(DECIMAL_PAIRS[pair1 * 2 + 0]);
            |                                                 ^^^^^^^^^^^^^ help: consider reducing it to: `(pair1 * 2)`
            |
            = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#identity_op
    
        warning: this operation has no effect
           --> src/lib.rs:384:49
            |
        384 |             buf[offset + 2].write(DECIMAL_PAIRS[pair2 * 2 + 0]);
            |                                                 ^^^^^^^^^^^^^ help: consider reducing it to: `(pair2 * 2)`
            |
            = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#identity_op
    
        warning: this operation has no effect
           --> src/lib.rs:404:17
            |
        404 |             buf[offset + 0].write(DECIMAL_PAIRS[pair * 2 + 0]);
            |                 ^^^^^^^^^^ help: consider reducing it to: `offset`
            |
            = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#identity_op
    
        warning: this operation has no effect
           --> src/lib.rs:404:49
            |
        404 |             buf[offset + 0].write(DECIMAL_PAIRS[pair * 2 + 0]);
            |                                                 ^^^^^^^^^^^^ help: consider reducing it to: `(pair * 2)`
            |
            = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#identity_op
    
        warning: this operation has no effect
           --> src/lib.rs:445:13
            |
        445 |         buf[quad_index * 4 + OFFSET + 0].write(DECIMAL_PAIRS[pair1 * 2 + 0]);
            |             ^^^^^^^^^^^^^^^^^^^^^^^^^^^ help: consider reducing it to: `(quad_index * 4 + OFFSET)`
            |
            = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#identity_op
    
        warning: this operation has no effect
           --> src/lib.rs:445:62
            |
        445 |         buf[quad_index * 4 + OFFSET + 0].write(DECIMAL_PAIRS[pair1 * 2 + 0]);
            |                                                              ^^^^^^^^^^^^^ help: consider reducing it to: `(pair1 * 2)`
            |
            = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#identity_op
    
        warning: this operation has no effect
           --> src/lib.rs:447:62
            |
        447 |         buf[quad_index * 4 + OFFSET + 2].write(DECIMAL_PAIRS[pair2 * 2 + 0]);
            |                                                              ^^^^^^^^^^^^^ help: consider reducing it to: `(pair2 * 2)`
            |
            = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#identity_op

--- src/lib.rs
+++ src/lib.rs
@@ -35,6 +35,7 @@
     clippy::cast_possible_truncation,
     clippy::cast_sign_loss,
     clippy::expl_impl_clone_on_copy,
+    clippy::identity_op,
     clippy::must_use_candidate,
     clippy::needless_doctest_main,
     clippy::unreadable_literal
