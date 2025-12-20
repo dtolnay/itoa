afd36af1b472fa2d63539024aa09dcf9882fecb9
David Tolnay <dtolnay@gmail.com>
2025-12-19 21:18:09 -0800

    Switch from cargo bench to criterion

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,57 +1,43 @@
-#![feature(test)]
-#![allow(non_snake_case)]
 #![allow(clippy::cast_lossless)]
 
-extern crate test;
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::fmt::Display;
+use std::hint;
+use std::io::Write;
 
-macro_rules! benches {
-    ($($name:ident($value:expr))*) => {
-        mod bench_itoa_format {
-            use std::hint;
-            use test::Bencher;
-
-            $(
-                #[bench]
-                fn $name(b: &mut Bencher) {
-                    let mut buffer = itoa::Buffer::new();
-
-                    b.iter(|| {
-                        let printed = buffer.format(hint::black_box($value));
-                        hint::black_box(printed);
-                    });
-                }
-            )*
-        }
-
-        mod bench_std_fmt {
-            use std::hint;
-            use std::io::Write;
-            use test::Bencher;
-
-            $(
-                #[bench]
-                fn $name(b: &mut Bencher) {
-                    let mut buf = Vec::with_capacity(40);
-
-                    b.iter(|| {
-                        buf.clear();
-                        write!(&mut buf, "{}", hint::black_box($value)).unwrap();
-                        hint::black_box(&buf);
-                    });
-                }
-            )*
-        }
-    }
+fn do_bench(c: &mut Criterion, group_name: &str, int: impl itoa::Integer + Display) {
+    let mut group = c.benchmark_group(group_name);
+    group.bench_function("itoa", |b| {
+        let mut buf = itoa::Buffer::new();
+        b.iter(move || {
+            let int = hint::black_box(int);
+            let formatted = buf.format(int);
+            hint::black_box(formatted);
+        });
+    });
+    group.bench_function("std::fmt", |b| {
+        let mut buf = Vec::with_capacity(20);
+        b.iter(|| {
+            buf.clear();
+            let int = hint::black_box(int);
+            write!(&mut buf, "{int}").unwrap();
+            hint::black_box(buf.as_slice());
+        });
+    });
+    group.finish();
 }
 
-benches! {
-    bench_u64_0(0u64)
-    bench_u64_half(u32::MAX as u64)
-    bench_u64_max(u64::MAX)
+fn bench(c: &mut Criterion) {
+    do_bench(c, "u64[0]", 0u64);
+    do_bench(c, "u64[half]", u32::MAX as u64);
+    do_bench(c, "u64[max]", u64::MAX);
 
-    bench_i16_0(0i16)
-    bench_i16_min(i16::MIN)
+    do_bench(c, "i16[0]", 0i16);
+    do_bench(c, "i16[min]", i16::MIN);
 
-    bench_u128_0(0u128)
-    bench_u128_max(u128::MAX)
+    do_bench(c, "u128[0]", 0u128);
+    do_bench(c, "u128[max]", u128::MAX);
 }
+
+criterion_group!(benches, bench);
+criterion_main!(benches);
