2a261f9f3e001fa05dd8bb7663122ec5614920e2
David Tolnay <dtolnay@gmail.com>
2025-12-20 12:15:06 -0800

    Sync algorithm from rust-lang/rust master

--- src/lib.rs
+++ src/lib.rs
@@ -40,11 +40,12 @@
     clippy::unreadable_literal
 )]
 
-mod udiv128;
+mod u128_ext;
 
 use core::hint;
 use core::mem::MaybeUninit;
-use core::{ptr, slice, str};
+use core::ptr;
+use core::str;
 #[cfg(feature = "no-panic")]
 use no_panic::no_panic;
 
@@ -122,101 +123,58 @@ mod private {
     }
 }
 
-const DEC_DIGITS_LUT: [u8; 200] = *b"\
-      0001020304050607080910111213141516171819\
-      2021222324252627282930313233343536373839\
-      4041424344454647484950515253545556575859\
-      6061626364656667686970717273747576777879\
-      8081828384858687888990919293949596979899";
-
-// Adaptation of the original implementation at
-// https://github.com/rust-lang/rust/blob/b8214dc6c6fc20d0a660fb5700dca9ebf51ebe89/src/libcore/fmt/num.rs#L188-L266
 macro_rules! impl_Integer {
-    ($t:ty as $large_unsigned:ty) => {
-        impl Integer for $t {
-            #[allow(unused_comparisons)]
-            const MAX_STR_LEN: usize = <$t>::MAX.ilog10() as usize + 1 + (<$t>::MIN < 0) as usize;
+    ($Signed:ident, $Unsigned:ident) => {
+        const _: () = {
+            assert!($Signed::MIN < 0, "need signed");
+            assert!($Unsigned::MIN == 0, "need unsigned");
+            assert!($Signed::BITS == $Unsigned::BITS, "need counterparts");
+        };
+
+        impl Integer for $Unsigned {
+            const MAX_STR_LEN: usize = $Unsigned::MAX.ilog10() as usize + 1;
         }
 
-        impl private::Sealed for $t {
+        impl private::Sealed for $Unsigned {
             type Buffer = [MaybeUninit<u8>; Self::MAX_STR_LEN];
 
-            #[allow(unused_comparisons)]
             #[inline]
             #[cfg_attr(feature = "no-panic", no_panic)]
             fn write(self, buf: &mut Self::Buffer) -> &str {
-                let is_nonnegative = self >= 0;
-                let mut n = if is_nonnegative {
-                    self as $large_unsigned
-                } else {
-                    // Convert negative number to positive by summing 1 to its two's complement.
-                    (!(self as $large_unsigned)).wrapping_add(1)
-                };
-                let mut curr = buf.len();
-                let buf_ptr = buf.as_mut_ptr().cast::<u8>();
-                let lut_ptr = DEC_DIGITS_LUT.as_ptr();
+                let offset = Unsigned::fmt(self, buf);
+                // SAFETY: Starting from `offset`, all elements of the slice have been set.
+                unsafe { slice_buffer_to_str(buf, offset) }
+            }
+        }
 
-                // Render 4 digits at a time.
-                while n >= 10000 {
-                    let rem = n % 10000;
-                    n /= 10000;
+        impl Integer for $Signed {
+            const MAX_STR_LEN: usize = $Signed::MAX.ilog10() as usize + 2;
+        }
 
-                    let d1 = ((rem / 100) << 1) as usize;
-                    let d2 = ((rem % 100) << 1) as usize;
-                    curr -= 4;
-                    unsafe {
-                        ptr::copy_nonoverlapping(lut_ptr.add(d1), buf_ptr.add(curr), 2);
-                        ptr::copy_nonoverlapping(lut_ptr.add(d2), buf_ptr.add(curr + 2), 2);
-                    }
+        impl private::Sealed for $Signed {
+            type Buffer = [MaybeUninit<u8>; Self::MAX_STR_LEN];
+
+            #[inline]
+            #[cfg_attr(feature = "no-panic", no_panic)]
+            fn write(self, buf: &mut Self::Buffer) -> &str {
+                let (prefix, unsigned) = buf.split_last_chunk_mut().unwrap();
+                let mut offset = prefix.len() + Unsigned::fmt(self.unsigned_abs(), unsigned);
+                if self < 0 {
+                    offset -= 1;
+                    buf[offset].write(b'-');
                 }
-
-                // Render 2 more digits, if >2 digits.
-                if n >= 100 {
-                    let d1 = ((n % 100) << 1) as usize;
-                    n /= 100;
-                    curr -= 2;
-                    unsafe {
-                        ptr::copy_nonoverlapping(lut_ptr.add(d1), buf_ptr.add(curr), 2);
-                    }
-                }
-
-                // Render last 1 or 2 digits.
-                if n < 10 {
-                    curr -= 1;
-                    unsafe {
-                        *buf_ptr.add(curr) = (n as u8) + b'0';
-                    }
-                } else {
-                    let d1 = (n << 1) as usize;
-                    curr -= 2;
-                    unsafe {
-                        ptr::copy_nonoverlapping(lut_ptr.add(d1), buf_ptr.add(curr), 2);
-                    }
-                }
-
-                if !is_nonnegative {
-                    curr -= 1;
-                    unsafe {
-                        *buf_ptr.add(curr) = b'-';
-                    }
-                }
-
-                let len = buf.len() - curr;
-                let bytes = unsafe { slice::from_raw_parts(buf_ptr.add(curr), len) };
-                unsafe { str::from_utf8_unchecked(bytes) }
+                // SAFETY: Starting from `offset`, all elements of the slice have been set.
+                unsafe { slice_buffer_to_str(buf, offset) }
             }
         }
     };
 }
 
-impl_Integer!(i8 as u32);
-impl_Integer!(u8 as u32);
-impl_Integer!(i16 as u32);
-impl_Integer!(u16 as u32);
-impl_Integer!(i32 as u32);
-impl_Integer!(u32 as u32);
-impl_Integer!(i64 as u64);
-impl_Integer!(u64 as u64);
+impl_Integer!(i8, u8);
+impl_Integer!(i16, u16);
+impl_Integer!(i32, u32);
+impl_Integer!(i64, u64);
+impl_Integer!(i128, u128);
 
 macro_rules! impl_Integer_size {
     ($t:ty as $primitive:ident #[cfg(target_pointer_width = $width:literal)]) => {
@@ -245,87 +203,245 @@ impl_Integer_size!(usize as u32 #[cfg(target_pointer_width = "32")]);
 impl_Integer_size!(isize as i64 #[cfg(target_pointer_width = "64")]);
 impl_Integer_size!(usize as u64 #[cfg(target_pointer_width = "64")]);
 
-macro_rules! impl_Integer128 {
-    ($t:ty) => {
-        impl Integer for $t {
-            #[allow(unused_comparisons)]
-            const MAX_STR_LEN: usize = <$t>::MAX.ilog10() as usize + 1 + (<$t>::MIN < 0) as usize;
-        }
+// The string of all two-digit numbers in range 00..99 is used as a lookup table.
+static DECIMAL_PAIRS: &[u8; 200] = b"\
+      0001020304050607080910111213141516171819\
+      2021222324252627282930313233343536373839\
+      4041424344454647484950515253545556575859\
+      6061626364656667686970717273747576777879\
+      8081828384858687888990919293949596979899";
 
-        impl private::Sealed for $t {
-            type Buffer = [MaybeUninit<u8>; Self::MAX_STR_LEN];
+/// This function converts a slice of ascii characters into a `&str` starting
+/// from `offset`.
+///
+/// # Safety
+///
+/// `buf` content starting from `offset` index MUST BE initialized and MUST BE
+/// ascii characters.
+unsafe fn slice_buffer_to_str(buf: &[MaybeUninit<u8>], offset: usize) -> &str {
+    // SAFETY: `offset` is always included between 0 and `buf`'s length.
+    let written = unsafe { buf.get_unchecked(offset..) };
+    // SAFETY: (`assume_init_ref`) All buf content since offset is set.
+    // SAFETY: (`from_utf8_unchecked`) Writes use ASCII from the lookup table exclusively.
+    unsafe { str::from_utf8_unchecked(written.assume_init_ref()) }
+}
 
-            #[allow(unused_comparisons)]
-            #[inline]
-            #[cfg_attr(feature = "no-panic", no_panic)]
-            fn write(self, buf: &mut Self::Buffer) -> &str {
-                let is_nonnegative = self >= 0;
-                let n = if is_nonnegative {
-                    self as u128
-                } else {
-                    // Convert negative number to positive by summing 1 to its two's complement.
-                    (!(self as u128)).wrapping_add(1)
-                };
-                let mut curr = buf.len();
-                let buf_ptr = buf.as_mut_ptr().cast::<u8>();
+trait Unsigned: Integer {
+    fn fmt(self, buf: &mut Self::Buffer) -> usize;
+}
 
-                // Divide by 10^19 which is the highest power less than 2^64.
-                let (n, rem) = udiv128::udivmod_1e19(n);
-                let buf1 = unsafe {
-                    buf_ptr
-                        .add(curr - u64::MAX_STR_LEN)
-                        .cast::<[MaybeUninit<u8>; u64::MAX_STR_LEN]>()
-                };
-                curr -= rem.write(unsafe { &mut *buf1 }).len();
+macro_rules! impl_Unsigned {
+    ($Unsigned:ident) => {
+        impl Unsigned for $Unsigned {
+            fn fmt(self, buf: &mut Self::Buffer) -> usize {
+                // Count the number of bytes in buf that are not initialized.
+                let mut offset = buf.len();
+                // Consume the least-significant decimals from a working copy.
+                let mut remain = self;
 
-                if n != 0 {
-                    // Memset the base10 leading zeros of rem.
-                    let target = buf.len() - 19;
-                    unsafe {
-                        ptr::write_bytes(buf_ptr.add(target), b'0', curr - target);
-                    }
-                    curr = target;
+                // Format per four digits from the lookup table.
+                // Four digits need a 16-bit $Unsigned or wider.
+                while size_of::<Self>() > 1
+                    && remain
+                        > 999
+                            .try_into()
+                            .expect("branch is not hit for types that cannot fit 999 (u8)")
+                {
+                    // SAFETY: All of the decimals fit in buf due to MAX_DEC_N
+                    // and the while condition ensures at least 4 more decimals.
+                    unsafe { core::hint::assert_unchecked(offset >= 4) }
+                    // SAFETY: The offset counts down from its initial buf.len()
+                    // without underflow due to the previous precondition.
+                    unsafe { core::hint::assert_unchecked(offset <= buf.len()) }
+                    offset -= 4;
 
-                    // Divide by 10^19 again.
-                    let (n, rem) = udiv128::udivmod_1e19(n);
-                    let buf2 = unsafe {
-                        buf_ptr
-                            .add(curr - u64::MAX_STR_LEN)
-                            .cast::<[MaybeUninit<u8>; u64::MAX_STR_LEN]>()
-                    };
-                    curr -= rem.write(unsafe { &mut *buf2 }).len();
-
-                    if n != 0 {
-                        // Memset the leading zeros.
-                        let target = buf.len() - 38;
-                        unsafe {
-                            ptr::write_bytes(buf_ptr.add(target), b'0', curr - target);
-                        }
-                        curr = target;
-
-                        // There is at most one digit left
-                        // because u128::MAX / 10^19 / 10^19 is 3.
-                        curr -= 1;
-                        unsafe {
-                            *buf_ptr.add(curr) = (n as u8) + b'0';
-                        }
-                    }
+                    // pull two pairs
+                    let scale: Self = 1_00_00
+                        .try_into()
+                        .expect("branch is not hit for types that cannot fit 1E4 (u8)");
+                    let quad = remain % scale;
+                    remain /= scale;
+                    let pair1 = (quad / 100) as usize;
+                    let pair2 = (quad % 100) as usize;
+                    buf[offset + 0].write(DECIMAL_PAIRS[pair1 * 2 + 0]);
+                    buf[offset + 1].write(DECIMAL_PAIRS[pair1 * 2 + 1]);
+                    buf[offset + 2].write(DECIMAL_PAIRS[pair2 * 2 + 0]);
+                    buf[offset + 3].write(DECIMAL_PAIRS[pair2 * 2 + 1]);
                 }
 
-                if !is_nonnegative {
-                    curr -= 1;
-                    unsafe {
-                        *buf_ptr.add(curr) = b'-';
-                    }
+                // Format per two digits from the lookup table.
+                if remain > 9 {
+                    // SAFETY: All of the decimals fit in buf due to MAX_DEC_N
+                    // and the if condition ensures at least 2 more decimals.
+                    unsafe { core::hint::assert_unchecked(offset >= 2) }
+                    // SAFETY: The offset counts down from its initial buf.len()
+                    // without underflow due to the previous precondition.
+                    unsafe { core::hint::assert_unchecked(offset <= buf.len()) }
+                    offset -= 2;
+
+                    let pair = (remain % 100) as usize;
+                    remain /= 100;
+                    buf[offset + 0].write(DECIMAL_PAIRS[pair * 2 + 0]);
+                    buf[offset + 1].write(DECIMAL_PAIRS[pair * 2 + 1]);
                 }
 
-                let len = buf.len() - curr;
-                let bytes = unsafe { slice::from_raw_parts(buf_ptr.add(curr), len) };
-                unsafe { str::from_utf8_unchecked(bytes) }
+                // Format the last remaining digit, if any.
+                if remain != 0 || self == 0 {
+                    // SAFETY: All of the decimals fit in buf due to MAX_DEC_N
+                    // and the if condition ensures (at least) 1 more decimals.
+                    unsafe { core::hint::assert_unchecked(offset >= 1) }
+                    // SAFETY: The offset counts down from its initial buf.len()
+                    // without underflow due to the previous precondition.
+                    unsafe { core::hint::assert_unchecked(offset <= buf.len()) }
+                    offset -= 1;
+
+                    // Either the compiler sees that remain < 10, or it prevents
+                    // a boundary check up next.
+                    let last = (remain & 15) as usize;
+                    buf[offset].write(DECIMAL_PAIRS[last * 2 + 1]);
+                    // not used: remain = 0;
+                }
+
+                offset
             }
         }
     };
 }
 
-impl_Integer128!(i128);
-impl_Integer128!(u128);
+impl_Unsigned!(u8);
+impl_Unsigned!(u16);
+impl_Unsigned!(u32);
+impl_Unsigned!(u64);
+
+impl Unsigned for u128 {
+    fn fmt(self, buf: &mut Self::Buffer) -> usize {
+        // Optimize common-case zero, which would also need special treatment due to
+        // its "leading" zero.
+        if self == 0 {
+            let offset = buf.len() - 1;
+            buf[offset].write(b'0');
+            return offset;
+        }
+        // Take the 16 least-significant decimals.
+        let (quot_1e16, mod_1e16) = div_rem_1e16(self);
+        let (mut remain, mut offset) = if quot_1e16 == 0 {
+            (mod_1e16, u128::MAX_STR_LEN)
+        } else {
+            // Write digits at buf[23..39].
+            enc_16lsd::<{ u128::MAX_STR_LEN - 16 }>(buf, mod_1e16);
+
+            // Take another 16 decimals.
+            let (quot2, mod2) = div_rem_1e16(quot_1e16);
+            if quot2 == 0 {
+                (mod2, u128::MAX_STR_LEN - 16)
+            } else {
+                // Write digits at buf[7..23].
+                enc_16lsd::<{ u128::MAX_STR_LEN - 32 }>(buf, mod2);
+                // Quot2 has at most 7 decimals remaining after two 1e16 divisions.
+                (quot2 as u64, u128::MAX_STR_LEN - 32)
+            }
+        };
+
+        // Format per four digits from the lookup table.
+        while remain > 999 {
+            // SAFETY: All of the decimals fit in buf due to u128::MAX_STR_LEN
+            // and the while condition ensures at least 4 more decimals.
+            unsafe { core::hint::assert_unchecked(offset >= 4) }
+            // SAFETY: The offset counts down from its initial buf.len()
+            // without underflow due to the previous precondition.
+            unsafe { core::hint::assert_unchecked(offset <= buf.len()) }
+            offset -= 4;
+
+            // pull two pairs
+            let quad = remain % 1_00_00;
+            remain /= 1_00_00;
+            let pair1 = (quad / 100) as usize;
+            let pair2 = (quad % 100) as usize;
+            buf[offset + 0].write(DECIMAL_PAIRS[pair1 * 2 + 0]);
+            buf[offset + 1].write(DECIMAL_PAIRS[pair1 * 2 + 1]);
+            buf[offset + 2].write(DECIMAL_PAIRS[pair2 * 2 + 0]);
+            buf[offset + 3].write(DECIMAL_PAIRS[pair2 * 2 + 1]);
+        }
+
+        // Format per two digits from the lookup table.
+        if remain > 9 {
+            // SAFETY: All of the decimals fit in buf due to u128::MAX_STR_LEN
+            // and the if condition ensures at least 2 more decimals.
+            unsafe { core::hint::assert_unchecked(offset >= 2) }
+            // SAFETY: The offset counts down from its initial buf.len()
+            // without underflow due to the previous precondition.
+            unsafe { core::hint::assert_unchecked(offset <= buf.len()) }
+            offset -= 2;
+
+            let pair = (remain % 100) as usize;
+            remain /= 100;
+            buf[offset + 0].write(DECIMAL_PAIRS[pair * 2 + 0]);
+            buf[offset + 1].write(DECIMAL_PAIRS[pair * 2 + 1]);
+        }
+
+        // Format the last remaining digit, if any.
+        if remain != 0 {
+            // SAFETY: All of the decimals fit in buf due to u128::MAX_STR_LEN
+            // and the if condition ensures (at least) 1 more decimals.
+            unsafe { core::hint::assert_unchecked(offset >= 1) }
+            // SAFETY: The offset counts down from its initial buf.len()
+            // without underflow due to the previous precondition.
+            unsafe { core::hint::assert_unchecked(offset <= buf.len()) }
+            offset -= 1;
+
+            // Either the compiler sees that remain < 10, or it prevents
+            // a boundary check up next.
+            let last = (remain & 15) as usize;
+            buf[offset].write(DECIMAL_PAIRS[last * 2 + 1]);
+            // not used: remain = 0;
+        }
+        offset
+    }
+}
+
+// Encodes the 16 least-significant decimals of n into `buf[OFFSET..OFFSET + 16]`.
+fn enc_16lsd<const OFFSET: usize>(buf: &mut [MaybeUninit<u8>], n: u64) {
+    // Consume the least-significant decimals from a working copy.
+    let mut remain = n;
+
+    // Format per four digits from the lookup table.
+    for quad_index in (0..4).rev() {
+        // pull two pairs
+        let quad = remain % 1_00_00;
+        remain /= 1_00_00;
+        let pair1 = (quad / 100) as usize;
+        let pair2 = (quad % 100) as usize;
+        buf[quad_index * 4 + OFFSET + 0].write(DECIMAL_PAIRS[pair1 * 2 + 0]);
+        buf[quad_index * 4 + OFFSET + 1].write(DECIMAL_PAIRS[pair1 * 2 + 1]);
+        buf[quad_index * 4 + OFFSET + 2].write(DECIMAL_PAIRS[pair2 * 2 + 0]);
+        buf[quad_index * 4 + OFFSET + 3].write(DECIMAL_PAIRS[pair2 * 2 + 1]);
+    }
+}
+
+// Euclidean division plus remainder with constant 1E16 basically consumes 16
+// decimals from n.
+//
+// The integer division algorithm is based on the following paper:
+//
+//   T. Granlund and P. Montgomery, “Division by Invariant Integers Using Multiplication”
+//   in Proc. of the SIGPLAN94 Conference on Programming Language Design and
+//   Implementation, 1994, pp. 61–72
+//
+#[inline]
+fn div_rem_1e16(n: u128) -> (u128, u64) {
+    const D: u128 = 1_0000_0000_0000_0000;
+    // The check inlines well with the caller flow.
+    if n < D {
+        return (0, n as u64);
+    }
+
+    // These constant values are computed with the CHOOSE_MULTIPLIER procedure
+    // from the Granlund & Montgomery paper, using N=128, prec=128 and d=1E16.
+    const M_HIGH: u128 = 76624777043294442917917351357515459181;
+    const SH_POST: u8 = 51;
+
+    // n.widening_mul(M_HIGH).1 >> SH_POST
+    let quot = u128_ext::mulhi(n, M_HIGH) >> SH_POST;
+    let rem = n - quot * D;
+    (quot, rem as u64)
+}
